05afabd202eb153591d0cd50d2abb0c5c2320a88
Théo Friberg <theo.friberg@kielikone.fi>
2023-10-12 16:22:53 +0300

    Improve cross building
    Can now produce an rpath'ed linux .so with only the haskell deps next to it in a subdirectory

--- cp_deps.sh
+++ cp_deps.sh
@@ -4,12 +4,15 @@ cp $(dirname $(stack exec -- which saldo-exe))/../lib/libsaldo.* lib/
 
 if [ "$(uname)" = "Linux" ]; then
 
-	echo "Fishing out dependencies for linux"
+	echo 'Fishing out dependencies for linux'
 
 	mkdir -p lib/ext_libs
 
-	ldd lib/libsaldo.so |grep ' => ' |cut -d '>' -f2 |cut -d '(' -f1 | while read file
+	ldd lib/libsaldo.so |grep ' => ' |grep 'libHS' |cut -d '>' -f2 |cut -d '(' -f1 | while read file
 		do
 			cp $file lib/ext_libs/
 		done
+	echo 'Editing rpath such that the main libsaldo.so can find its Haskell deps'
+
+	chrpath -r '$ORIGIN/ext_libs' lib/libsaldo.so
 fi
